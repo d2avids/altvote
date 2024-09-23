26a6f11d42ef62703ac0c47ab9d199cfb8e9fe01
David Saidov <delightxxls@gmail.com>
2024-09-24 01:00:00 +0300

    customize admin header and title

--- users/admin.py
+++ users/admin.py
@@ -6,3 +6,8 @@ from django.contrib import admin
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name')
     search_fields = ('username', 'email', 'first_name', 'last_name')
+
+
+admin.AdminSite.site_header = 'AltVote Administration'
+admin.AdminSite.site_title = 'AltVote Administration'
+admin.AdminSite.index_title = 'AltVote'
